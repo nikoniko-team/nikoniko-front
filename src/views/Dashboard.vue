a82be5ccb05b69343d50856734deea2511277e76
Thiago Silva <thiago0112@gmail.com>
2019-12-03 20:37:10 -0300

    Adiciona template do admin

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <AdminLayout>
+    <div class="Dashboard">
+      <pre v-text="$attrs"/>
+    </div>
+  </AdminLayout>
+</template>
+
+<script>
+import AdminLayout from '@/components/AdminLayout.vue';
+
+export default {
+  props: {},
+  components: { AdminLayout },
+};
+</script>
