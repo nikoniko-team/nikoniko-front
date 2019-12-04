76279afc2e0d3b3783e4774f35b2f33f1072d465
Thiago Silva <thiago0112@gmail.com>
2019-12-03 22:57:00 -0300

    Adiciona graficos no dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,32 @@
 <template>
   <AdminLayout>
-    <CommentsCard />
+    <v-row dense>
+      <v-col cols="6">
+        <LatestMonthsCard />
+      </v-col>
+      <v-col cols="6">
+        <MonthlyChartCard />
+      </v-col>
+      <v-col cols="12">
+        <CommentsCard />
+      </v-col>
+    </v-row>
   </AdminLayout>
 </template>
 
 <script>
 import AdminLayout from '@/components/AdminLayout.vue';
 import CommentsCard from '@/components/Dashboard/CommentsCard.vue';
+import MonthlyChartCard from '@/components/Dashboard/MonthlyChartCard.vue';
+import LatestMonthsCard from '@/components/Dashboard/LatestChartCard.vue';
 
 export default {
   props: {},
-  components: { AdminLayout, CommentsCard },
+  components: {
+    AdminLayout,
+    CommentsCard,
+    MonthlyChartCard,
+    LatestMonthsCard,
+  },
 };
 </script>
