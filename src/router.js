cc4616c4563f427e789e9ebf04c8b10f01589440
Thiago Silva <thiago0112@gmail.com>
2019-12-03 23:07:43 -0300

    Adiciona pagina de login

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Dashboard from '@/views/Dashboard.vue';
+import Login from '@/views/Login.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/admin',
       name: 'dashboard',
       component: Dashboard,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
   ],
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            md="4"
+            sm="8"
+          >
+            <ValidationObserver ref="form" v-slot="{ invalid, validated, passes }">
+              <v-card class="elevation-12">
+                <v-toolbar
+                  color="primary"
+                  dark
+                  flat
+                >
+                  <v-toolbar-title>Login</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-alert
+                    v-model="alert"
+                    dense
+                    outlined
+                    type="error"
+                    dismissible
+                  >
+                    Usuário ou senha incorretos
+                  </v-alert>
+                  <v-form>
+                    <VTextFieldWithValidation
+                      icon="mdi-email"
+                      label="E-mail"
+                      rules="required|email"
+                      type="email"
+                      vid="email"
+                      autocomplete="username"
+                      v-model="user.email"
+                    />
+
+                    <VTextFieldWithValidation
+                      icon="mdi-lock"
+                      label="Senha"
+                      rules="required|min:3"
+                      type="password"
+                      vid="password"
+                      autocomplete="new-password"
+                      v-model="user.password"/>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer/>
+                  <v-btn color="primary" :disabled="invalid || !validated">Login</v-btn>
+                </v-card-actions>
+              </v-card>
+            </ValidationObserver>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import { ValidationObserver } from 'vee-validate';
+import VTextFieldWithValidation from '@/components/Inputs/VTextFieldWithValidation.vue';
+
+export default {
+  components: {
+    ValidationObserver,
+    VTextFieldWithValidation,
+  },
+  data: () => ({
+    user: {},
+    alert: false,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
